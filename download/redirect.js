22000a9c9ea78940719f87399e1a837d65c9f449
Taras Petruk <taras.petruk@gmail.com>
2019-12-03 19:12:30 +0200

    test window.close()

--- download/redirect.js
+++ download/redirect.js
@@ -3,7 +3,8 @@ var pathName = window.location.pathname;
 if (pathName.startsWith(basePath)) {
   var platformChannel = pathName.substring(basePath.length);
   if (platformChannel == 'windows64-dev/') {
-    //window.location.href = 'https://github.com/toggl-open-source/toggldesktop/releases/download/v7.4.1023/TogglDesktopInstaller-7.4.1023.exe';
-    window.location.replace('https://github.com/toggl-open-source/toggldesktop/releases/download/v7.4.1023/TogglDesktopInstaller-7.4.1023.exe');
+    window.location.href = 'https://github.com/toggl-open-source/toggldesktop/releases/download/v7.4.1023/TogglDesktopInstaller-7.4.1023.exe';
+    //window.location.replace('https://github.com/toggl-open-source/toggldesktop/releases/download/v7.4.1023/TogglDesktopInstaller-7.4.1023.exe');
+    window.close();
   }
 }

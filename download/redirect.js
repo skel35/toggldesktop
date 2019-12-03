687c2800fcb186096f970580e36366f142f2d86e
Taras Petruk <taras.petruk@gmail.com>
2019-12-03 19:40:20 +0200

    clean up

--- download/redirect.js
+++ download/redirect.js
@@ -3,10 +3,9 @@ var pathName = window.location.pathname;
 if (pathName.startsWith(basePath)) {
   var platformChannel = pathName.substring(basePath.length);
   if (platformChannel == 'windows64-dev/') {
-    window.location.href = 'https://github.com/toggl-open-source/toggldesktop/releases/download/v7.4.1023/TogglDesktopInstaller-7.4.1023.exe';
-    //window.location.replace('https://github.com/toggl-open-source/toggldesktop/releases/download/v7.4.1023/TogglDesktopInstaller-7.4.1023.exe');
+    location.href = 'https://github.com/toggl-open-source/toggldesktop/releases/download/v7.4.1023/TogglDesktopInstaller-7.4.1023.exe';
     setTimeout(() => {
-      window.close();
+      close();
     }, 4000);
   }
 }

6a31181257a8826f72baeb6126c97edd77a0b13f
Taras Petruk <taras.petruk@gmail.com>
2019-12-03 18:54:28 +0200

    redirect windows64-dev

--- download/redirect.js
+++ download/redirect.js
@@ -1,1 +1,8 @@
-console.log(window.location.pathname);
+var basePath = '/toggldesktop/download/';
+var pathName = window.location.pathname;
+if (pathName.startsWith(basePath) {
+  var platformChannel = pathName.substring(basePath.length);
+  if (platformChannel == 'windows64-dev/') {
+    window.location.href = 'https://github.com/toggl-open-source/toggldesktop/releases/download/v7.4.1023/TogglDesktopInstaller-7.4.1023.exe';
+  }
+}
